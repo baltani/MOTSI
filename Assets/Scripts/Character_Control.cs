c855df037791553a28cb5b02db868c7ea5d67d70
Baltani <baltani@student.ethz.ch>
2020-04-17 11:45:20 +0200

    Animation

--- Assets/Scripts/Character_Control.cs
+++ Assets/Scripts/Character_Control.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Character_Control : MonoBehaviour
 {
+
+    public float Speed = 3f;
+    public float jump = 6f;
+    public float gravity = 1.5f; //bruchemer das???
+    public Camera mainCamera;
+
+
+ 
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public Transform firePoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+
+    }
+
+}
