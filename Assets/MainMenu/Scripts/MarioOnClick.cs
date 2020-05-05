5c818ff532aea53434018acf70f4f9d9f0897e1d
ITikkaMyChikka22 <siramesh@student.ethz.ch>
2020-05-05 09:56:00 +0200

    Added Main Menu, Added 2 seperate scenes for 1v1 and run mode

---
+++ Assets/MainMenu/Scripts/MarioOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MarioOnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        //sceneName = "SampleScene";
+        SceneManager.LoadScene(sceneName);
+    }
+}
